a4c3803415f55d24aeb4136477209998e9673a72
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2019-12-24 02:22:09 +0900

    add built files for Github Pages

---
+++ build/precache-manifest.f5b397e6357a885febda7308ff5f62e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695f618f85b147629f11b1c6662eb4cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce4333231fc485c677aa",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "48f07545da7947092698",
+    "url": "/static/js/2.bbd5dc40.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.bbd5dc40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce4333231fc485c677aa",
+    "url": "/static/js/main.fcef4690.chunk.js"
+  },
+  {
+    "revision": "26c4859ea5e9e9c2cb58",
+    "url": "/static/js/runtime-main.33e4ac08.js"
+  }
+]);
